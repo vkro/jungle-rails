333aa7e43fc77fe1cca71aff9b445d150e508b33
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-12 21:59:32 +0000

    changed from index to show as per assignment instructions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :about, only: [:index]
+  resource :about, only: [:show]
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
