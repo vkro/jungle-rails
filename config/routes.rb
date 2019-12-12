3bf79f818f422a1def416b24b2f44252ebae704e
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-12 21:27:46 +0000

    change about route to only index to omit :id requirement

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :about, only: [:show]
+  resources :about, only: [:index]
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
