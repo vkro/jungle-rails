50092077440f0dd0c75607594d66c3e5dcce99f4
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-13 18:15:43 +0000

    get & post routes for user signup

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, :categories, except: [:edit, :update, :show]
