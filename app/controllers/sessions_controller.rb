3804795853c37c62b2300568d4afcfc36ca419a1
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-13 18:57:53 +0000

    session controller logic to log users in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    # if the user exists AND the password entered is correct
+    if user && user.authenticate(params[:password])
+      # save the user id inside the browser cookie to keep the user logged in
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      # if user's login doesn't work, send them back to login form
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
-  
+
 end
