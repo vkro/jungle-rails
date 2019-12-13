8a7db9961a717d2173eca541eeaf69a344bc1fc3
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-13 02:09:55 +0000

    fill in admin category controller, add views

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,32 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["WEBSITE_USERNAME"], password: ENV["WEBSITE_PASSWORD"]
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:product).permit(
+      :name,
+      :products,
+    )
+  end
+
 end
