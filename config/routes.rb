e84134ab8448ff18597e105ee508fcd80f6381f3
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-13 18:20:32 +0000

    generate users controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # receive the form and create a user in our database using the data given to us by the user.
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
-
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, :categories, except: [:edit, :update, :show]
