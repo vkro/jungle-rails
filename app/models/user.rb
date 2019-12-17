d63de38cc53c8a3f101b957fbcd4edc46ced3ae8
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-17 22:42:50 +0000

    downcase & remove whitespace from email before save; add nil return value to authenticate_with method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  def authenticate_with_credentials(email, password)
+    User.find_by(email: email.strip.downcase).try(:authenticate, password) || nil
+  end
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
@@ -9,9 +13,18 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 8 }
   validates :password_confirmation, presence: true 
 
+  private
+  
+  before_save :remove_whitespace, :to_lower_case  
 
-  def authenticate_with_credentials(email, password)
-    User.find_by(email: email).try(:authenticate, password)
-  end
+    def remove_whitespace
+      email.strip
+    end
+
+    def to_lower_case
+      email.downcase
+    end
+
+
 
 end
