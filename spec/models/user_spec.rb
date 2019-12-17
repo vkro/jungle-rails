91d8c95e453fdc6963df4c3bc15f15076f74a072
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-17 03:45:04 +0000

    reorganize and change test case to new class instance for several tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,21 +9,10 @@ RSpec.describe User, type: :model do
     it 'is valid with valid attributes' do
       expect(subject).to be_valid
     end
-
-    it 'is not valid if password_confirmation does not match password field' do
-      subject.password_confirmation = 'xdpassword'
+        
+    it 'is not valid without a password' do
+      subject.password = nil
       expect(subject).not_to be_valid
-    end
-
-    it 'cannot be created if password is less than 8 characters' do
-      subject.password = 'passwor'
-      subject.password_confirmation = 'passwor'
-      expect(subject).not_to be_valid
-    end
-
-    it 'cannot be created without matching password and password_confirmation' do
-      new_user = User.new(first_name: "name", last_name: 'ln', email: 'me@me.com', password: 'password', password_confirmation: 'passw')
-      expect(new_user).not_to be_valid
     end
 
     it 'cannot be created without a password' do
@@ -35,9 +24,19 @@ RSpec.describe User, type: :model do
       new_user = User.new(first_name: "name", last_name: 'ln', email: 'me@me.com', password: 'password', password_confirmation: nil)
       expect(new_user).not_to be_valid
     end
-    
-    it 'is not valid without a password' do
-      subject.password = nil
+
+    it 'cannot be created if password is less than 8 characters' do
+      new_user = User.new(first_name: "name", last_name: 'ln', email: 'me@me.com', password: 'passwor', password_confirmation: 'passwor')
+      expect(new_user).not_to be_valid
+    end
+
+    it 'is not valid if password_confirmation does not match password field' do
+      subject.password_confirmation = 'xdpassword'
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without an email' do
+      subject.email = nil
       expect(subject).not_to be_valid
     end
 
@@ -46,11 +45,6 @@ RSpec.describe User, type: :model do
       user2 = described_class.create(first_name: 'b', last_name: 'c', email: 'abc@def.com', password: 'password1', password_confirmation: 'password1')
       user3 = described_class.create(first_name: 'c', last_name: 'd', email: 'aBc@def.com', password: 'password2', password_confirmation: 'password2')
       expect(user2).not_to be_valid
-    end
-
-    it 'is not valid without an email' do
-      subject.email = nil
-      expect(subject).not_to be_valid
     end
 
     it 'is not valid without a first name' do
