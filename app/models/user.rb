9a7b8a7765fb12061bc43c845627a226b88930d6
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-17 03:10:31 +0000

    add password length validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates :password, length: { minimum: 8 }
 
 end
