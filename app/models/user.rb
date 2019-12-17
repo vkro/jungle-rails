62db2d5e49edc3bf7e5ef60a7113e59c07b59705
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-17 03:36:32 +0000

    add uniqueness validation for user email field

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates_uniqueness_of :email
   validates :password, length: { minimum: 8 }
   validates :password_confirmation, presence: true 
 
