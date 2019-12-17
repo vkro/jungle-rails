271eecb5a7dd7e7ada2462b832488d61f0984756
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-17 04:54:39 +0000

    add case insensitive to email uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,13 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :case_sensitive => false
   validates :password, length: { minimum: 8 }
   validates :password_confirmation, presence: true 
+
+
+  def authenticate_with_credentials(email, password)
+    User.find_by(email: email).try(:authenticate, password)
+  end
 
 end
