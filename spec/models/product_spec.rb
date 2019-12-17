89f9a5e28ba6bf36b19eadffc201456986094639
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-17 01:58:23 +0000

    test: product is valid with valid attributes

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'Validations' do
+
+    it 'is valid with valid attributes' do
+      test_cat = Category.new(name: "test category")
+      expect(Product.new(name: 'anything', price_cents: 1, quantity: 1, category: test_cat)).to be_valid
+    end
+
+    
+    it 'is not valid without a name'
+    # validates :name, presence: true
+
+    it 'is not valid without a price'
+
+    #validates :price, presence: true
+
+    it 'is not valid without a quantity'
+    #validates :quantitiy, presence: true
+
+    it 'is not valid without a category'
+    #validates :category, presence: true
+
+
+  end
+
 end
