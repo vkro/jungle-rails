814f0b94489f0c95deb90abe7c7a08edf1c144ec
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-18 01:29:17 +0000

    feature test - verify user can navigate to product detail page from home page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to product details page", type: :feature, js: true do
+    
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "From home page, they click on product details button, and see the product page" do
+    # ACT
+    visit root_path
+    click_on('Details', match: :first)
+
+    # DEBUG / VERIFY
+    expect(page).to have_css('section.products-show')
+    save_screenshot
+  end
+
+
+
+
 end
