8c82f75beff8fcb4b614c1a3eadfff81a22a8a58
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-18 21:03:22 +0000

    added 'user = User.find...' back to fix login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
     # if the user exists AND the password entered is correct
     if user = User.authenticate_with_credentials(params[:email], params[:password])
       # save the user id inside the browser cookie to keep the user logged in
+
       session[:user_id] = user.id
       redirect_to '/'
     else
