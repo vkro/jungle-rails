a413f1749f16632920f8ac3973e7db5a4aa611bb
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-18 23:08:11 +0000

    refactor duplicate code in product_spec with before :each

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,39 +4,39 @@ RSpec.describe Product, type: :model do
   
   describe 'Validations' do
 
-    it 'is valid with valid attributes' do
+    before :each do
       @category = Category.new(name: "test category")
-      expect(Product.new(name: 'anything', price_cents: 1, quantity: 1, category: @category)).to be_valid
+      @product = Product.new(name: 'product name', price_cents: 1, quantity: 1, category: @category)
+    end
+
+    it 'is valid with valid attributes' do
+      expect(@product).to be_valid
     end
     
     it 'is not valid without a name' do
-      @category = Category.new(name: "test category")
-      @product = Product.new(name: nil, price_cents: 1, quantity: 1, category: @category)
+      @product.name = nil
       expect(@product).not_to be_valid
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'is not valid without a price' do
-      @category = Category.new(name: "test category")
-      @product = Product.new(name: 'product name', price_cents: nil, quantity: 1, category: @category)
+      @product.price_cents = nil
       expect(@product).not_to be_valid
       expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'is not valid without a quantity' do
-      @category = Category.new(name: "test category")
-      @product = Product.new(name: 'product name', price_cents: 1, quantity: nil, category: @category)
+      @product.quantity = nil
       expect(@product).not_to be_valid
       expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'is not valid without a category' do
-      @product = Product.new(name: 'product name', price_cents: 1, quantity: nil, category: nil)
+      @product.category = nil
       expect(@product).not_to be_valid
-      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
 
-
   end
-
+  
 end
