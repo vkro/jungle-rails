a79d203f82a5709c26c307cabb63e88cbb50a778
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-18 02:01:02 +0000

    feature test - verify user can add item to cart, cart increases by 1

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.feature "When visitor clicks shopping-cart 'Add' button from home page, cart increases by one", type: :feature, js: true do
+    
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "From home page, they click on shopping-cart button, and see the cart count increase by one" do
+    # ACT
+    visit root_path
+    expect(page).to have_content('My Cart (0)')
+    save_screenshot
+    # async - waits until at least one element matching selector appears on page
+    click_on('Add', match: :first)
+
+    # DEBUG / VERIFY
+    # async - as above
+    expect(page).to have_content('My Cart (1)')
+    save_screenshot
+  end
+
+
+
+
+end
